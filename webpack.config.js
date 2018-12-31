0757bdd216cefd7b8c277857137c29b5b493c38d
seymakara <seymakara88@gmail.com>
2018-12-31 01:26:41 -0800

    update webpack related settings

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,20 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
+  mode: 'development',
+  devtool: 'inline-source-map',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
       }
-    ]
+    ],
   }
 };
