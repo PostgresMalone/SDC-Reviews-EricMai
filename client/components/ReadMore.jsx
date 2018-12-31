2bde2b93df23dcad18d3e67960846cb304915c80
seymakara <seymakara88@gmail.com>
2018-12-30 20:44:35 -0800

    apply css-in-js

--- client/components/ReadMore.jsx
+++ client/components/ReadMore.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Review from './Review.jsx';
+import style from './style.css.js';
 
 const ReadMore = (props) => {
   if (props.showMoreButton) {
     return (
-      <a href='#' onClick={props.handleSwitch}>Show all reviews</a>
+      <a href='#' style={style.showReviews} onClick={props.handleSwitch}>Show all reviews</a>
     )
   } else {
     return (
-      <a href='#' onClick={props.handleSwitch}>Hide all reviews</a>
+      <a href='#' style={style.showReviews} onClick={props.handleSwitch}>Hide all reviews</a>
     )
   }
 };
