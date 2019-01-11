ee929a72ff9f06f32f8310c9a065d9de2990a843
ericmai25 <maieric25@gmail.com>
2019-01-11 15:44:50 -0800

    Fixed variables and removed unneeded comments

--- ReviewSeeder.js
+++ ReviewSeeder.js
@@ -1,32 +1,15 @@
-const stringify = require("csv-stringify");
 const fs = require("fs");
 const faker = require("faker");
 
-let randomName = faker.name.findName;
-let content = faker.lorem.paragraph;
-let sentence = faker.lorem.sentences;
-let reviewerName = faker.name.findName;
-let hostComment = faker.lorem.paragraph;
-let createdAt = faker.date.recent;
-let randomNumber = faker.random.number;
-let now = Date.now() / 1000;
-let start = 0;
+const sentence = faker.lorem.sentences;
+const createdAt = faker.date.recent;
+const randomNumber = faker.random.number;
+const now = Date.now() / 1000;
+const start = 0;
 //10mil- host
 //30mil- user
 //300mill- comments
-const ExampleData = {
-  id: 1,
-  listingId: 1,
-  content:
-    "Commodi vel aut accusantium. Eveniet omnis quas occaecati culpa eum quam. Et officiis et. Et tempora in quam magnam dolore. Maxime consequuntur vero est.",
-  reviewerName: "Adriana Reynolds",
-  reviewerPicture: "https://randomuser.me/api/portraits/men/23.jpg",
-  hostComment:
-    "Dolor occaecati quibusdam sint consequatur est sunt deserunt. Accusantium asperiores consectetur exercitationem. Reprehenderit sit eaque. Itaque optio rerum rem eum. Sequi porro quia nam beatae cumque. Enim incidunt est fugiat inventore.",
-  hostName: "River DuBuque",
-  hostPicture: "https://randomuser.me/api/portraits/men/39.jpg",
-  createdAt: "2019-01-05T08:01:18.798Z"
-};
+
 let data = ()=>{
   //max 10000000
     let roomId = randomNumber({ min: 0, max: 1000 });
@@ -41,7 +24,7 @@ let data = ()=>{
       createdAt()
     ].join()+'\n';
 } ;
-let column = ["id","listingId","content","userid","hostComment", "createdAt"]
+const column = ["id","listingId","content","userid","hostComment", "createdAt"]
 //100mil
 //2000
 for (let j = 0; j < 1; j++) {
--- UserSeeder.js
+++ UserSeeder.js
@@ -1,32 +1,14 @@
-const stringify = require("csv-stringify");
 const fs = require("fs");
 const faker = require("faker");
 
-let randomName = faker.name.findName;
-let content = faker.lorem.paragraph;
-let sentence = faker.lorem.sentences;
-let ranNames = faker.name.findName;
-let hostComment = faker.lorem.paragraph;
-let createdAt = faker.date.recent;
-let randomNumber = faker.random.number;
-let now = Date.now() / 1000;
-let start = 0;
+const ranNames = faker.name.findName;
+const createdAt = faker.date.recent;
+const randomNumber = faker.random.number;
+const now = Date.now() / 1000;
+const start = 0;
 //10mil- host
 //30mil- user
 //300mill- comments
-// const ExampleData = {
-//   id: 1,
-//   listingId: 1,
-//   content:
-//     "Commodi vel aut accusantium. Eveniet omnis quas occaecati culpa eum quam. Et officiis et. Et tempora in quam magnam dolore. Maxime consequuntur vero est.",
-//   reviewerName: "Adriana Reynolds",
-//   reviewerPicture: "https://randomuser.me/api/portraits/men/23.jpg",
-//   hostComment:
-//     "Dolor occaecati quibusdam sint consequatur est sunt deserunt. Accusantium asperiores consectetur exercitationem. Reprehenderit sit eaque. Itaque optio rerum rem eum. Sequi porro quia nam beatae cumque. Enim incidunt est fugiat inventore.",
-//   hostName: "River DuBuque",
-//   hostPicture: "https://randomuser.me/api/portraits/men/39.jpg",
-//   createdAt: "2019-01-05T08:01:18.798Z"
-// };
 let data = () => {
   let picture = randomNumber({ min: 0, max: 99 });
   let gender = ["men", "women"];
@@ -41,7 +23,7 @@ let data = () => {
     ].join() + "\n"
   );
 };
-let column = ['id', 'userName', 'userPicture', 'createdAt'];
+const column = ['id', 'userName', 'userPicture', 'createdAt'];
 //10mil
 
 for (let j = 0; j < 1; j++) {
@@ -62,7 +44,6 @@ function writeOneMillionTimes(writer, data) {
         start++;
       } else {
         ok = writer.write(data());
-        //   console.log(ok)
         start++;
       }
     }
